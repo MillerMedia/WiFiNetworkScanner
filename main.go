362520c062ceefaaa786c4495ecb79eb69fee0a2
Kali Live user <kali@localhost>
2024-05-04 16:43:55 -0700

    Updates to main.go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -15,25 +17,50 @@ func checkWebPage(ip string, debug bool, wg *sync.WaitGroup, results chan<- stri
 	defer wg.Done()
 	defer func() { <-semaphore }()
 
-	url := fmt.Sprintf("http://%s:80", ip)
-	client := http.Client{
-		Timeout: 5 * time.Second,
-	}
+	protocols := []string{"http", "https"}
+	successful := false
 
-	response, err := client.Get(url)
-	defer func() {
+	for _, protocol := range protocols {
+		url := fmt.Sprintf("%s://%s", protocol, ip)
+		client := http.Client{
+			Timeout: 10 * time.Second,
+		}
+
+		if protocol == "http" {
+			// Disable redirect following for the "http" protocol
+			client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			}
+		}
+
+		if protocol == "https" {
+			// Skip TLS verification for the "https" protocol
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			client.Transport = tr
+		}
+
+		response, err := client.Get(url)
+		if err == nil && response.StatusCode >= 200 && response.StatusCode < 400 {
+			successful = true
+			results <- fmt.Sprintf("%s [\033[32m✓\033[0m] (%s)", ip, protocol)
+			fmt.Printf("%s [\033[32m✓\033[0m]\n", ip)
+		} else if debug {
+			fmt.Printf("%s [\033[31mX\033[0m] (%s) - url: %s - error: %v\n", ip, protocol, url, err)
+		}
+
 		if response != nil {
 			response.Body.Close()
 		}
-	}()
 
-	if err == nil {
-		results <- fmt.Sprintf("%s [\033[32m✓\033[0m]", ip)
-		fmt.Printf("%s [\033[32m✓\033[0m]\n", ip)
-	} else {
-		if debug {
-			fmt.Printf("%s [\033[31mX\033[0m]\n", ip)
+		if successful {
+			break
 		}
+	}
+
+	if !successful && debug {
+		fmt.Printf("%s [\033[31mX\033[0m]\n", ip)
 	}
 }
 
@@ -41,16 +68,18 @@ func scanRange(networkRange string, start, end int, debug bool, results chan<- s
 	var wg sync.WaitGroup
 	semaphore := make(chan struct{}, maxConcurrent)
 
-	for i := start; i <= end; i++ {
-		ip := fmt.Sprintf("%s.%d", networkRange, i)
-		wg.Add(1)
-		semaphore <- struct{}{}
-		go checkWebPage(ip, debug, &wg, results, semaphore)
+	for x := start; x <= end; x++ {
+		for y := 1; y <= 254; y++ {
+			ip := fmt.Sprintf("%s.%d.%d", networkRange, x, y)
+			wg.Add(1)
+			semaphore <- struct{}{}
+			go checkWebPage(ip, debug, &wg, results, semaphore)
+		}
 	}
 	wg.Wait()
 }
 
-func getLocalIPRange() string {
+func getLocalIPRanges() []string {
 	cmd := exec.Command("ifconfig")
 	if _, err := exec.LookPath("ifconfig"); err != nil {
 		cmd = exec.Command("ip", "addr")
@@ -59,26 +88,44 @@ func getLocalIPRange() string {
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error running command:", err)
-		return ""
+		return nil
 	}
 
+	var ranges []string
 	lines := strings.Split(string(output), "\n")
+	var currentInterface string
+
+	ifaceRegex := regexp.MustCompile(`^(\S+):`)
+
 	for _, line := range lines {
+		if matches := ifaceRegex.FindStringSubmatch(line); matches != nil {
+			currentInterface = matches[1]
+		}
+
 		if strings.Contains(line, "inet ") && !strings.Contains(line, "127.0.0.1") {
 			fields := strings.Fields(line)
+			if len(fields) < 2 {
+				continue
+			}
+
 			ip := fields[1]
-			ip = strings.Split(ip, "/")[0]
-			ipParts := strings.Split(ip, ".")
+			ipAddr := strings.Split(ip, "/")[0]
+			ipParts := strings.Split(ipAddr, ".")
+
+			if currentInterface == "tun0" || currentInterface == "docker0" || strings.HasPrefix(currentInterface, "virbr") || strings.HasPrefix(currentInterface, "veth") {
+				continue
+			}
+
 			if len(ipParts) == 4 {
-				return fmt.Sprintf("%s.%s.%s", ipParts[0], ipParts[1], ipParts[2])
+				ranges = append(ranges, fmt.Sprintf("%s.%s", ipParts[0], ipParts[1]))
 			}
 		}
 	}
-	return ""
+	return ranges
 }
 
 func runNmapOnLocalhost() {
-	cmd := exec.Command("nmap", "localhost")
+	cmd := exec.Command("nmap", "localhost", "")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
@@ -92,20 +139,11 @@ func main() {
 	maxConcurrent := flag.Int("maxconcurrent", 100, "Max concurrent requests")
 	flag.Parse()
 
-	localIPRange := getLocalIPRange()
-	if localIPRange == "" {
-		fmt.Println("Could not determine the local IP range.")
+	fmt.Println("Determining local IP ranges.")
+	localIPRanges := getLocalIPRanges()
+	if localIPRanges == nil {
+		fmt.Println("Could not determine any local IP ranges.")
 		return
-	}
-
-	fmt.Println("Scanning local IP range:", localIPRange)
-
-	_, err := exec.LookPath("nmap")
-	if err == nil {
-		fmt.Println("Nmap found, running a port scan on localhost...")
-		runNmapOnLocalhost()
-	} else {
-		fmt.Println("Nmap not found, skipping port scan on localhost.")
 	}
 
 	results := make(chan string)
@@ -123,7 +161,14 @@ func main() {
 		}
 	}()
 
-	scanRange(localIPRange, 1, 254, *debug, results, *maxConcurrent)
+	for _, localIPRange := range localIPRanges {
+		fmt.Printf("Scanning local IP range: %s.0.0/16\n", localIPRange)
+		if strings.HasSuffix(localIPRange, ".0/24") {
+			scanRange(strings.TrimSuffix(localIPRange, ".0/24"), 0, 0, *debug, results, *maxConcurrent)
+		} else {
+			scanRange(localIPRange, 0, 255, *debug, results, *maxConcurrent)
+		}
+	}
 
 	close(results)
 	wg.Wait()
